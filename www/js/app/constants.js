e3ba3fb3a38272ffca97f8b2f41b578b225d53cd
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2015-12-24 20:53:56 +0100

    added constants class

---
+++ www/js/app/constants.js
@@ -0,0 +1,14 @@
+'use strict';
+(function () {
+    var constants = {
+        problemTypes: {
+            OTHER: 0,
+            
+            ACCIDENT: 1,
+            BREAKDOWN: 2,
+            TRAFFIC_JAM: 3
+        }
+    };
+    
+    window.app.constants = constants;
+})();
